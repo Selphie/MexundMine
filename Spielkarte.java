f132edaec7f5f2c315eaa9e25795f8238adb88d5
Mark Fichtner <m.fichtner@wiss-ki.eu>
2014-03-13 18:08:15 +0100

    simplification
    :D

--- Spielkarte.java
+++ Spielkarte.java
@@ -1,14 +1,20 @@
 
 public class Spielkarte {
 	
+	int wertOben;
+	int wertUnten;
+	int wertLinks;
+	int wertRechts;
+	
+	/* Wertzuweisung kommt dann im Konstruktor ^^
 	int[][] karte = new int[3][3];
 	
 	karte[0][1] = 5;
 	karte[1][0] = 3;
 	karte[1][2] = 3;
 	karte[2][1] = 2;
-	
-	System.out.println("   "+karte[0][1]+"\n"+karte[1][0]+"     "+karte[1][2]+"\n"+"   "+karte[2][1]);
+	*/
+	// ohne main kann man nicht System.out.println("   "+karte[0][1]+"\n"+karte[1][0]+"     "+karte[1][2]+"\n"+"   "+karte[2][1]);
 
 	
 }

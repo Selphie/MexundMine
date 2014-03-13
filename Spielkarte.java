b9f97c5a855af65c8f521721fc937d400933b441
Myriel Fichtner <Miniminzi@gmx.de>
2014-03-13 18:23:52 +0100

    Getter&Setter
    Getter und Setter Methoden hinzugefügt.

--- Spielkarte.java
+++ Spielkarte.java
@@ -1,20 +1,38 @@
 
 public class Spielkarte {
-	
+
 	int wertOben;
 	int wertUnten;
 	int wertLinks;
 	int wertRechts;
-	
-	/* Wertzuweisung kommt dann im Konstruktor ^^
-	int[][] karte = new int[3][3];
-	
-	karte[0][1] = 5;
-	karte[1][0] = 3;
-	karte[1][2] = 3;
-	karte[2][1] = 2;
-	*/
-	// ohne main kann man nicht System.out.println("   "+karte[0][1]+"\n"+karte[1][0]+"     "+karte[1][2]+"\n"+"   "+karte[2][1]);
 
-	
+	public static void main(String[] args){
+		//System.out.println("   "+karte[0][1]+"\n"+karte[1][0]+"     "+karte[1][2]+"\n"+"   "+karte[2][1]);
+	}
+
+	public int getwertOben(){
+		return wertOben;
+	}
+	public int getwertUnten(){
+		return wertUnten;	
+	}
+	public int getwertLinks(){
+		return wertLinks;
+	}	
+	public int getwertRechts(){
+		return wertRechts;
+	}
+
+	public void setwertOben(int x){
+		wertOben = x;
+	}
+	public void setwertUnten(int x){
+		wertUnten = x;
+	}
+	public void setwertLinks(int x){
+		wertLinks = x;
+	}
+	public void setwertRechts(int x){
+		wertRechts = x;
+	}
 }

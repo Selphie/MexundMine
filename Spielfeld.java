88dc50ff99c14c1c516f3eaa1693d82aa627294d
Myriel Fichtner <Miniminzi@gmx.de>
2014-03-13 17:25:36 +0100

    Array veraendert
    Array wird jetzt als 3x3 Spielflaeche angezeigt :)

--- Spielfeld.java
+++ Spielfeld.java
@@ -1,26 +1,25 @@
-﻿/**
- * Hauptklasse für das Spielfeld
- * @author Myriel Fichtner, Mark Fichtner
- */
+
 public class Spielfeld {
 	
-	// Hauptfunktion
 	public static void main (String[] args){
-		
+
 		// Spielfeld initialisieren
-		int[][] feld = new int[2][2];
+		int[][] feld = new int[3][3];
 		
 		// Durch das Spielfeld iterieren
 		for(int i = 0; i<3; i++)
 		{
 			// zuerst x-Koordinate, dann Y-Koordinate
 			for(int j=0; j<3; j++){
-				// Hier tun wir irgendwas komisches... ich weiss ned so recht was
-				feld[i][j] = i;
-				feld[i][j] = j;
-				// und ich weiss auch ned, wieso man das da tut ^^
-				System.out.println(i+j);
-			}
-		}
+				
+					System.out.print(i +""+ j+"  ");
+					
+				if(j==2){
+					System.out.println("");
+						}
+				}
+				
+		 }
 	}
+
 }

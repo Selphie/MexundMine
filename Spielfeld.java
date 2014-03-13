5dd9954a3f2132dda43b572a797584c945b0114e
Mark Fichtner <m.fichtner@wiss-ki.eu>
2014-03-13 16:45:13 +0100

    Documentation
    Dokumentation hinzugefügt, Dateiformat auf UTF-8 verändert.

--- Spielfeld.java
+++ Spielfeld.java
@@ -1,18 +1,26 @@
-
+﻿/**
+ * Hauptklasse für das Spielfeld
+ * @author Myriel Fichtner, Mark Fichtner
+ */
 public class Spielfeld {
 	
+	// Hauptfunktion
 	public static void main (String[] args){
-
-	int[][] feld = new int[2][2];
-	
-	for(int i = 0; i<3; i++)
-	{
-		for(int j=0; j<3; j++){
-			feld[i][j] = i;
-			feld[i][j] = j;
-			System.out.println(i+j);
+		
+		// Spielfeld initialisieren
+		int[][] feld = new int[2][2];
+		
+		// Durch das Spielfeld iterieren
+		for(int i = 0; i<3; i++)
+		{
+			// zuerst x-Koordinate, dann Y-Koordinate
+			for(int j=0; j<3; j++){
+				// Hier tun wir irgendwas komisches... ich weiss ned so recht was
+				feld[i][j] = i;
+				feld[i][j] = j;
+				// und ich weiss auch ned, wieso man das da tut ^^
+				System.out.println(i+j);
+			}
 		}
-
 	}
-}
 }

b46f9742d4fe7e7f3a8000e2973a188b2beec816
Myriel Fichtner <Miniminzi@gmx.de>
2014-03-13 18:04:42 +0100

    nochmal weng anders

--- Spielkarte.java
+++ Spielkarte.java
@@ -1,16 +1,14 @@
 
 public class Spielkarte {
 	
+	int[][] karte = new int[3][3];
+	
 	karte[0][1] = 5;
 	karte[1][0] = 3;
 	karte[1][2] = 3;
 	karte[2][1] = 2;
 	
-	public static void main(String[] args){
-		// Karte initalisieren
-		int[][] karte = new int[3][3];
+	System.out.println("   "+karte[0][1]+"\n"+karte[1][0]+"     "+karte[1][2]+"\n"+"   "+karte[2][1]);
 
-		System.out.println("   "+karte[0][1]+"\n"+karte[1][0]+"     "+karte[1][2]+"\n"+"   "+karte[2][1]);
-		}
 	
 }
